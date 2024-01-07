95aea16b531ceef1ed352954098d339aeae207ac
virtual-float <krystianturek5@outlook.com>
2024-01-07 16:53:35 +0100

    Kod podstawowy

---
+++ bitservice.py
@@ -0,0 +1,12 @@
+# Importowanie modułow niezbędnych do uruchomienia gry
+import tkinter, pygame, json
+import asyncio, os
+from tkinter import messagebox
+
+
+async def main():
+    messagebox.showinfo('Informacja', 'uruchomiono pomyślnie')
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
