203a43d2987df0d122c603a1c402ee1dc7c9098e
virtual-float <krystianturek5@outlook.com>
2024-01-07 17:14:38 +0100

    Ustawienia, odczytywanie/zapis json

---
+++ bin/function.py
@@ -0,0 +1,56 @@
+# Importuj moduły niezbędne
+import os, json
+from tkinter import messagebox
+
+# Tryb kodowania znaków [ string ] ( Zmienna stała )
+ENCODING_MODE : str = "UTF-8"
+
+# Funkcja, która umożliwia odczytywanie danych z struktury json 
+# Funkcja zwraca dane json w postaci słownika [ dict ]
+def readJSON(localizationPath: str) -> tuple[dict, bool]:
+    try:
+        # Jeżeli ścieżka pliku nie kończy się na .json, zwróć błąd
+        if not localizationPath.endswith(".json"):
+            messagebox.showerror('Rozszerzenie', 'Rozszerzenie pliku nie jest typu json!')
+
+        # Otwórz plik json 
+        with open(os.getcwd() + localizationPath, mode='r', encoding=ENCODING_MODE) as file:
+            # Jeżeli plik jest odczytywalny, zwróć dane z pliku json
+            if file.readable():
+                # Zwróć dane z pliku json jako słownik oraz wynik odczytania plik
+                # True  - Plik został poprawnie odczytany
+                # False - Plik nie został poprawne odczytany
+                return json.load(file), True
+            
+    except json.JSONDecodeError:
+        # Zwróć poprawny komunikat jeśli nastąpił błąd z dekodowaniem znaków
+        messagebox.showerror('Dekodowanie', 'Plik nie mógł zostać zdekodowany na UTF-8')
+    except OSError:
+        # Zwróć poprawny komunikat jeśli nastąpił błąd z otwarciem pliku
+        messagebox.showerror('Dostępność', 'Plik nie mógł być otworzony')
+    # Fabrycznie zwróć pusty słownik oraz wynik odczytania pliku na False
+    return {}, False
+
+# Funkcja, która umożliwia aktualizowanie danych do pliku json
+# Funkcja zwraca wynik wykonania aktualizacji
+def updateJSON(localizationPath: str, newData: dict) -> bool:
+    try:
+        # Jeżeli ścieżka pliku nie kończy się na .json, zwróć błąd
+        if not localizationPath.endswith(".json"):
+            messagebox.showerror('Rozszerzenie', 'Rozszerzenie pliku nie jest typu json!')
+
+        # Otwórz plik json
+        with open(os.getcwd() + localizationPath, mode='w', encoding=ENCODING_MODE) as file:
+            # Ustaw kursor pliku na pozycji początkowej (0)
+            file.seek(0)
+            # Wpisz nowe dane do pliku json
+            file.write(json.dumps(newData, indent=4))
+
+            # Zwróć wynik aktualizacji
+            return True
+    except OSError:
+        # Zwróć poprawny komunikat jeśli nastąpił błąd z otwarciem pliku
+        messagebox.showerror('Dostępność', 'Plik nie mógł być otworzony')
+    # Fabrycznie zwróć wynik aktualizacji danych json
+    return False
+
