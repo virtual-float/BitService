9165784d6925284b98cde469c0c6c45dd2292d90
lisiastek <patryklata11@gmail.com>
2024-01-31 20:15:28 +0100

    a ić z tym regexem

--- bin/templatewindows.py
+++ bin/templatewindows.py
@@ -2,6 +2,7 @@ import bin.window as wn
 
 # szatan tworzył regexa
 # ^[0-9]{1,3}\.?|[0-9]{3}\.[0-9]{0-3}\.?|[0-9]{3}\.[0-9]{3}\.[0-9]{0,3}|$
+# ^[0-9]{0,3}|[0-9]{3}\.|[0-9]{3}\.[0-9]{0,3}|[0-9]{3}\.[0-9]{3}\.|[0-9]{3}\.[0-9]{3}\.[0-9]{0,3}$
 
 def question(name: str) -> wn.window:
     _window = wn.window(name=name, size=(400,250), body=wn.windowBody(
@@ -9,7 +10,7 @@ def question(name: str) -> wn.window:
         wn.windowText(fontName='VERYSMALL_COMICSANS', text='Adres ip i jakieś coś', cords=(10, 45), color=(136,136,136)),
         wn.windowText(fontName='VERYSMALL_COMICSANS', text='Adres broadcast', cords=(10, 100)),
         textbox1 := wn.windowTextBox(startingText="", cords=(10, 140), xsize=30, marginleft=5, name="test")
-        .setRegex('^[0-9]{1,3}\.?|[0-9]{3}\.[0-9]{0,3}|[0-9]{3}.[0-9]{3}.|[0-9]{3}\.[0-9]{3}\.[0-9]{0,3}|$'),
+        .setRegex('^[0-9]{0,3}|[0-9]{3}\.|[0-9]{3}\.[0-9]{0,3}|[0-9]{3}\.[0-9]{3}\.|[0-9]{3}\.[0-9]{3}\.[0-9]{0,3}$'),
         wn.windowText(fontName='VERYSMALL_COMICSANS', text='ZATWIERDZ', cords=(265, 220)).addClickListener(
             lambda me, pressed, pos, posRelative, events, window: window.kill()
         )
