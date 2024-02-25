b7a16debe91f092cafd09b02638ad0a90e8a02a0
virtual-float <krystianturek5@outlook.com>
2024-02-25 18:22:14 +0100

    Zaaktualizowano grafiki

--- bitservice.py
+++ bitservice.py
@@ -429,7 +429,7 @@ async def main(gameSettings: dict):
             game.client.clientGroup.draw(display)
             
             # renderowanie efektów dla klientów
-            game.client.clientEffectGroup.draw(display)
+            game.client.clientEffectGroup.draw(display) 
 
             if kera.gameover:
                 pygame.mixer.music.stop()
